a2c13d3e7fd00357a552fa571d4eef338adb6b32
gabri <gabriel_winsper@hotmail.com>
2026-07-02 01:37:43 -0300

    beecrowd problems

---
+++ src/beecrowd/ex1000.java
@@ -0,0 +1,12 @@
+package beecrowd;
+
+public class ex1000 {
+    public static void main(String[]args){
+
+        System.out.println("Hello World!");
+
+
+
+
+    }
+}
---
+++ src/beecrowd/ex1002.java
@@ -0,0 +1,18 @@
+package beecrowd;
+import java.util.Scanner;
+
+public class ex1002 {
+public static void main(String[]args){
+    Scanner sc = new Scanner(System.in);
+
+    double n = 3.14159;
+    double x= sc.nextDouble();
+    double A = n*(x*x);
+
+    System.out.printf("A=%.4f%n",A);
+
+
+
+
+}
+}
