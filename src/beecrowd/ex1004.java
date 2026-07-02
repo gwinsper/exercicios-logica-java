a3a424ba253dc221572f923e5c2aca54ce3c4f24
gabri <gabriel_winsper@hotmail.com>
2026-07-02 03:01:33 -0300

    beecrowd problems

---
+++ src/beecrowd/ex1004.java
@@ -0,0 +1,28 @@
+/*Leia dois valores inteiros. A seguir, calcule o produto entre estes dois valores
+e atribua esta operação à variável PROD. A seguir mostre a variável PROD com mensagem correspondente.
+
+Entrada
+O arquivo de entrada contém 2 valores inteiros.
+
+Saída
+Imprima a mensagem "PROD" e a variável PROD conforme exemplo abaixo,
+ com um espaço em branco antes e depois da igualdade.
+  Não esqueça de imprimir o fim de linha após o produto,
+   caso contrário seu programa apresentará a mensagem: “Presentation Error”.*/
+
+package beecrowd;
+import java.util.Scanner;
+
+public class ex1004 {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+
+        int x =sc.nextInt();
+        int y =sc.nextInt();
+
+        int PROD = x * y;
+        System.out.println("PROD = "+PROD);
+
+
+    }
+}
